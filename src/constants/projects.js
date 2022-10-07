52006ba55c9ef8836cd31f377df77f73c3ced5c6
maximuusS <ksaurav0211@gmail.com>
2022-10-07 02:10:35 -0400

    Added new projects

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,129 +1,109 @@
 export const projects = [
-   
-    {
-      title: "Online Vehicle Requisition System",
+   {
+      title: 'Online Voting system using blockchain',
       details:
-        "A full stack web application, made for my 3rd year mini project in group of two, where users can customize their vehicle, view the 3d models and purchase it using RazorPay after uploading identity details which can be verified by admin. The website also has has user dashboard where users can see all the past orders and admin dashboard where admin can see all the users and see all the orders placed",
-      category: "Web Development",
+         'A secured online voting system built using MongoDB, ExpressJS, ReactJS, NodeJS (MERN), Solidity (Smart Contracts), Tailwind CSS and Web3JS. In this application, the admin can start elections, add candidates, manage users and end elections using admin panel. The user will able to vote and see all the results of previous elections. The results of elections held and votes are stored in blockchain database for secutiry',
+      category: 'Blockchain',
       technologies: [
-          "ReactJS",
-          "ReduxJS",
-          "Node.js",
-          "MongoDB",
-          "ExpressJS",
-          "Cloudinary",
-          "RazorPay",
-          "ThreeJS",
-          "MailTrap"
+         'ReactJS',
+         'Node.js',
+         'ExpressJS',
+         'MongoDB',
+         'Solidity',
+         'Infura',
+         'Ethereum-Blockchain',
+         'Smart contract',
+         'Web3.js',
+         'Mailtrap',
+         'TailWind CSS',
       ],
-      url: "https://github.com/itsSauravK/Online-Vehicle-Requisition-System",
-      id: "VehicleSystem",
-    },
-  
-    {
-      title: "E-Commerce Webiste",
+      url: 'https://github.com/itsSauravK/Election-Voting-Blockchain',
+      id: 'Voting system',
+   },
+   {
+      title: 'Online Vehicle Requisition System',
       details:
-        "A full stack E-Commerce website where users can search, filter, review products and add it to cart. It has a user and admin panel and a payment gateway",
-      category: "Web Development",
-      url: "",
-      id: "E-Commerce Website",
+         'A full stack web application, made for my 3rd year mini project in group of two, where users can customize their vehicle, view the 3d models and purchase it using RazorPay after uploading identity details which can be verified by admin. The website also has has user dashboard where users can see all the past orders and admin dashboard where admin can see all the users and see all the orders placed',
+      category: 'Web Development',
       technologies: [
-          "Node.js",
-          "ReactJS",
-          "ReduxJS",
-          "ExpressJS",
-          "MongoDB",
-          "Cloudinary",
-          "Stripe"
-      ]
-    },
-    {
-      title: "ShareMe",
-      details:
-        "Photo-sharing website for photographers wherein they can create their profile by signing up and can post photos. They can store photos in a gallery and share/commentThey can comment on photos",
-      technologies: [
-        "Bootstrap4",
-        "Node.js",
-        "MongoDB",
-        "ExpressJS",
-        "REST",
-        "connect-flash",
-        "PassportJS",
-        "Cloudinary",
+         'ReactJS',
+         'ReduxJS',
+         'Node.js',
+         'MongoDB',
+         'ExpressJS',
+         'Cloudinary',
+         'RazorPay',
+         'ThreeJS',
+         'MailTrap',
       ],
-      category: "Web Development",
+      url: 'https://github.com/itsSauravK/Online-Vehicle-Requisition-System',
+      id: 'VehicleSystem',
+   },
+   {
+      title: 'DNS and DNSSEC (CSE 534 - Computer Networks)',
+      details:
+         'Implemented iterative DNS using the dnspython library for resolving the requests from the root server for A, MX and NS records and extended the DNS resolver to use the security extension (DNSSEC) to validate the digital signature, RRsets, and zone (chain of trust) in a DNS response for A type requests',
+      category: 'Computer Networks',
+      technologies: ['python', 'dnspython', 'cryptography'],
+      url: '',
+      id: 'Voting system',
+   },
+   {
+      title: 'ShareMe',
+      details:
+         'Photo-sharing website for photographers wherein they can create their profile by signing up and can post photos. They can store photos in a gallery and share/commentThey can comment on photos',
+      technologies: [
+         'Bootstrap4',
+         'Node.js',
+         'MongoDB',
+         'ExpressJS',
+         'REST',
+         'connect-flash',
+         'PassportJS',
+         'Cloudinary',
+      ],
+      category: 'Web Development',
       images: [],
-      url: "https://github.com/itsSauravK/ShareMe",
-      id: "shareme",
-    },
-    {
-      title: "Online Voting system using blockchain",
+      url: 'https://github.com/itsSauravK/ShareMe',
+      id: 'shareme',
+   },
+
+   {
+      title: 'Kickstarter-using-Ethereum',
       details:
-        "Final year project, in group of three, to develop an online voting system using Ethereum Blockchain. (Still in progress)",
-      category: "Blockchain",
-      technologies: [
-          "ReactJS",
-          "Node.js",
-          "ExpressJS",
-          "MongoDB",
-          "Ethereum-Blockchain",
-          "Smart contract",
-          "Web3.js"
-      ],
-      url: "",
-      id: "Voting system",
-    },
-    
-    {
-      title: "Kickstarter-using-Ethereum",
-      details:"A blockchain based web application where users can create campaign and receive contributions from other users. If the user wants to spend the contributed ether, he has to create a request which needs to be approved by maximum contributors",
-      category: "Blockchain",
-      technologies: [
-        "Solidity",
-        "Smart Contracts",
-        "Mocha",
-        "web3.js",
-        "ReactJS",
-        "NextJS"
-      ],
-      url:"https://github.com/itsSauravK/Kickstarter-using-ethereum",
-      id:"Kickstarter-Using-Ethereum"
-    },
-    {
-      title: "YelpCamp",
+         'A blockchain based web application where users can create campaign and receive contributions from other users. If the user wants to spend the contributed ether, he has to create a request which needs to be approved by maximum contributors',
+      category: 'Blockchain',
+      technologies: ['Solidity', 'Smart Contracts', 'Mocha', 'web3.js', 'ReactJS', 'NextJS'],
+      url: 'https://github.com/itsSauravK/Kickstarter-using-ethereum',
+      id: 'Kickstarter-Using-Ethereum',
+   },
+   {
+      title: 'YelpCamp',
       details:
-        "A campground reservation portal to help users find available campgrounds and view their prices. Users can post/edit their campgrounds as well as comment on other campgrounds.",
+         'A campground reservation portal to help users find available campgrounds and view their prices. Users can post/edit their campgrounds as well as comment on other campgrounds.',
       technologies: [
-        "Bootstrap3",
-        "Node.js",
-        "MongoDB",
-        "PassportJS",
-        "MomentJS",
-        "ExpressJS",
-        "REST",
+         'Bootstrap3',
+         'Node.js',
+         'MongoDB',
+         'PassportJS',
+         'MomentJS',
+         'ExpressJS',
+         'REST',
       ],
-      category: "Web Development",
+      category: 'Web Development',
       images: [],
-      url: "https://github.com/itsSauravK/YelpCamp",
-      id: "yelpcamp",
-    },
-    {
-      title: "Blog Website",
-      details: "Full stack website where users can store and post their blogs",
-      technologies: [
-        "Semantic UI",
-        "Node.js",
-        "MongoDB",
-        "ExpressJS",
-        "REST",
-        "Express Sanitizer",
-      ],
-      category: "Web Development",
+      url: 'https://github.com/itsSauravK/YelpCamp',
+      id: 'yelpcamp',
+   },
+   {
+      title: 'Blog Website',
+      details: 'Full stack website where users can store and post their blogs',
+      technologies: ['Semantic UI', 'Node.js', 'MongoDB', 'ExpressJS', 'REST', 'Express Sanitizer'],
+      category: 'Web Development',
       images: [],
-      url: "https://github.com/itsSauravK/blogs",
-      id: "blogwebsite",
-    },
-  ];
-  // Populated using the API below:
-  // https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@ksaurav0211
-  
+      url: 'https://github.com/itsSauravK/blogs',
+      id: 'blogwebsite',
+   },
+];
+// Populated using the API below:
+// https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@ksaurav0211
